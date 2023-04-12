8a7d137ee9e145c22d871bbd004666e5fa07b13f
SolomanDavis <ziyad.yafi@gmail.com>
2023-04-12 10:21:07 -0400

    Attempt to intercept as completion stage

--- src/main/kotlin/live/bunch/interceptor/FaultyInterceptor.kt
+++ src/main/kotlin/live/bunch/interceptor/FaultyInterceptor.kt
@@ -1,19 +1,30 @@
 package live.bunch.interceptor
 
+import io.micronaut.aop.InterceptedMethod
 import io.micronaut.aop.InterceptorBean
 import io.micronaut.aop.MethodInterceptor
 import io.micronaut.aop.MethodInvocationContext
 import jakarta.inject.Singleton
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 @Singleton
 @InterceptorBean(FaultyInterceptorAnnotation::class)
 class FaultyInterceptor() : MethodInterceptor<Any?, Any?> {
     override fun intercept(context: MethodInvocationContext<Any?, Any?>): Any? {
+        val executorService = Executors.newSingleThreadExecutor()
+
         if (context.isSuspend) {
+            val interceptedMethod = InterceptedMethod.of(context)
             println("Attempting to intercept method ${context.methodName}")
 
             try {
-                val res = context.proceed()
+                val res = interceptedMethod.handleResult(
+                    CompletableFuture.supplyAsync(
+                        { interceptedMethod.interceptResultAsCompletionStage() },
+                        executorService
+                    )
+                )
                 println("Proceeded with context and got result $res")
                 return res
             } catch (ex: Throwable) {
